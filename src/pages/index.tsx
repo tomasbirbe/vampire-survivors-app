b720cd0d435106cb87102bbe7c65deac67e78a02
Tomas Birbe <tomas.birbe@gmail.com>
2022-06-05 22:36:05 -0300

    Dev: Make a basic layout to select passive items and weapons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { PassiveItem, Weapon } from 'src/types';
 
 import { Container, Stack, Text } from '@chakra-ui/react';
+import { useState } from 'react';
 
 interface Props {
   weapons: Weapon[];
@@ -8,17 +9,44 @@ interface Props {
 }
 
 export default function Home({ weapons, passiveItems }: Props) {
+  const [myWeapons, setMyWeapons] = useState<Weapon[]>([]);
+  const [myPassiveItems, setMyPassiveItems] = useState<PassiveItem[]>([]);
+
+  function addToMyWeapons(weapon: Weapon) {
+    setMyWeapons((prevState) => [...prevState, weapon]);
+  }
+
+  function addToMyPassiveItems(passiveItem: PassiveItem) {
+    setMyPassiveItems((prevState) => [...prevState, passiveItem]);
+  }
+
   return (
     <Container maxWidth="full">
+      <Stack align="center">
+        <Stack as="ul" direction="row">
+          {myWeapons.map((myWeapon) => (
+            <Text key={myWeapon.id}>{myWeapon.name}</Text>
+          ))}
+        </Stack>
+        <Stack as="ul" direction="row">
+          {myPassiveItems.map((myPassiveItem) => (
+            <Text key={myPassiveItem.id}>{myPassiveItem.name}</Text>
+          ))}
+        </Stack>
+      </Stack>
       <Stack direction="row" justify="space-around">
         <Stack as="ul">
           {weapons.map((weapon) => (
-            <Text key={weapon.name}>{weapon.name}</Text>
+            <Text key={weapon.name} onClick={() => addToMyWeapons(weapon)}>
+              {weapon.name}
+            </Text>
           ))}
         </Stack>
         <Stack as="ul">
           {passiveItems.map((passiveItem) => (
-            <Text key={passiveItem.name}>{passiveItem.name}</Text>
+            <Text key={passiveItem.name} onClick={() => addToMyPassiveItems(passiveItem)}>
+              {passiveItem.name}
+            </Text>
           ))}
         </Stack>
       </Stack>
